8ec1c53f16d01ed719cc8dab7325607b1968da54
unknown <annam.manoj1996@gmail.com>
2019-01-27 17:41:50 +0530

    ArrayMaxSum

---
+++ ArrayMaxConsecutiveSum/ArrayMaxConsecutiveSum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMaxConsecutiveSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 2, 3, 4, 5, 6, 7, 8 };
+            int k = 2;
+            int j = maxSum(a,k);
+            Console.WriteLine();
+        }
+
+        private static int maxSum(int[] inputArray, int k)
+        {
+            List<int> sumResults = new List<int>();
+            for(int i=0; i<=(inputArray.Length-k);i++)
+            {
+                int temp = k,sum = 0;int addVariable = i;
+                while(temp>0)
+                {
+                    sum += inputArray[addVariable];
+                    temp--;
+                    addVariable++;
+                }
+                sumResults.Add(sum);
+            }
+            return sumResults.Max();
+        }
+    }
+}

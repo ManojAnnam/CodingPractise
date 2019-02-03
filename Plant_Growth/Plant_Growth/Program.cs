afdffbae2922f5e069749da4cf312e6e06db463a
unknown <annam.manoj1996@gmail.com>
2019-02-03 20:59:34 +0530

    Plant_Growth_CodeFights

---
+++ Plant_Growth/Plant_Growth/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plant_Growth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 10;
+            int c = 2;
+            float f = (a - b) /(float) (b - c);
+            double d = Math.Ceiling(f + 1);
+            int res = Convert.ToInt32(d);
+            Console.WriteLine();
+        }
+    }
+}

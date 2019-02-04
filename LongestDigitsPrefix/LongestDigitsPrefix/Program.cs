c8ff88d7c514b2c5ff09feaf93223135675c0827
unknown <annam.manoj1996@gmail.com>
2019-02-04 07:14:22 +0530

    LongestDigitsPrefix@Codefights

---
+++ LongestDigitsPrefix/LongestDigitsPrefix/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LongestDigitsPrefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "123";
+            longestDigitsPrefix(inputString);
+            Console.ReadLine();
+        }
+
+        private static string longestDigitsPrefix(string inputString)
+        {
+            Regex onlyNumbers = new Regex("^[0-9]+$");
+            Regex onlyNonNumbers = new Regex("[^0-9]");
+            bool containsOnlyNonNumbers = onlyNonNumbers.IsMatch(inputString);
+            bool containsOnlyNumbers = onlyNumbers.IsMatch(inputString);
+            return null;
+
+        }
+    }
+}

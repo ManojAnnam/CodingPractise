2db235faacd330fbc8e844e8986aff108288c959
unknown <annam.manoj1996@gmail.com>
2019-02-05 09:12:40 +0530

    LongestDigitsPrefix@Codefights_Modified

--- LongestDigitsPrefix/LongestDigitsPrefix/Program.cs
+++ LongestDigitsPrefix/LongestDigitsPrefix/Program.cs
@@ -11,17 +11,54 @@ namespace LongestDigitsPrefix
     {
         static void Main(string[] args)
         {
-            string inputString = "123";
-            longestDigitsPrefix(inputString);
+
+            string inputString = "123abddef123as12345";
+            string result = longestDigitsPrefix(inputString);
             Console.ReadLine();
         }
 
         private static string longestDigitsPrefix(string inputString)
         {
             Regex onlyNumbers = new Regex("^[0-9]+$");
-            Regex onlyNonNumbers = new Regex("[^0-9]");
-            bool containsOnlyNonNumbers = onlyNonNumbers.IsMatch(inputString);
-            bool containsOnlyNumbers = onlyNumbers.IsMatch(inputString);
+            Regex onlyNonNumbers = new Regex("[\\D]");
+            Regex onlyAlphabet = new Regex("^[a-zA-Z]+$");
+            List<string> chunks = new List<string>();
+            string temp = string.Empty;
+            if (onlyNumbers.IsMatch(inputString))
+            {
+               // return inputString;
+            }
+            else if (onlyAlphabet.IsMatch(inputString))
+            {
+                 return null;
+            }
+            else
+            {
+                for(int i=0;i<inputString.Length;i++)
+                {
+                    while(i < inputString.Length && onlyNumbers.IsMatch(inputString[i].ToString()))
+                    {
+                        temp += inputString[i];
+                        i++;
+                    }
+                    if(temp!=string.Empty)
+                    {
+                        chunks.Add(temp);
+                        temp = string.Empty;
+                    }
+                    while (i < inputString.Length && onlyNonNumbers.IsMatch(inputString[i].ToString()))
+                    {
+                        temp += inputString[i];
+                        i++;
+                    }
+                    if (temp != string.Empty)
+                    {
+                        chunks.Add(temp);
+                        temp = string.Empty;
+                    }
+                    i--;
+                }
+            }
             return null;
 
         }

70f1ad37fbf764ddf74df310d1634b192a5d4c97
unknown <annam.manoj1996@gmail.com>
2019-02-11 09:59:40 +0530

    Pawn and Bishop

---
+++ BishopAndPawn/BishopAndPawn/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BishopAndPawn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bishop = string.Empty;
+            string pawn = string.Empty;
+            bishop = Console.ReadLine();
+            pawn = Console.ReadLine();
+            bishopAndPawn(bishop, pawn);
+          
+        }
+
+        private static bool bishopAndPawn(string bishop, string pawn)
+        {
+            int bishopInt = 0;
+            bishopInt = bishop[0];
+            int biInt = bishopInt - 96;
+            int pawnInt = 0;
+            pawnInt = pawn[0];
+            int paInt = pawnInt - 96;
+            bool loopVariable = true;
+
+            while (loopVariable)
+            {
+
+            }
+
+            while (loopVariable)
+            {
+
+            }
+
+            while (loopVariable)
+            {
+
+            }
+
+            while (loopVariable)
+            {
+
+            }
+
+
+            return false;
+        }
+    }
+}

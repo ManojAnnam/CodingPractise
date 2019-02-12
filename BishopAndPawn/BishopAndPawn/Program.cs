a0b832702682f28137d3536cda16c600c46faf98
unknown <annam.manoj1996@gmail.com>
2019-02-12 08:13:00 +0530

    Pawn and Bishop1

--- BishopAndPawn/BishopAndPawn/Program.cs
+++ BishopAndPawn/BishopAndPawn/Program.cs
@@ -14,8 +14,8 @@ namespace BishopAndPawn
             string pawn = string.Empty;
             bishop = Console.ReadLine();
             pawn = Console.ReadLine();
-            bishopAndPawn(bishop, pawn);
-          
+            Console.WriteLine(bishopAndPawn(bishop, pawn));
+            Console.ReadLine();
         }
 
         private static bool bishopAndPawn(string bishop, string pawn)
@@ -23,32 +23,88 @@ namespace BishopAndPawn
             int bishopInt = 0;
             bishopInt = bishop[0];
             int biInt = bishopInt - 96;
+            bishopInt = Convert.ToInt32(bishop[1].ToString());
             int pawnInt = 0;
             pawnInt = pawn[0];
             int paInt = pawnInt - 96;
-            bool loopVariable = true;
-
-            while (loopVariable)
+            pawnInt = Convert.ToInt32(pawn[1].ToString()); 
+            if (bishopInt == pawnInt || biInt == paInt)
             {
-
+                return false;
+            }
+            else if (paInt > biInt && pawnInt > bishopInt)
+            {
+                return IncreasingColumnIncreasingRow(biInt, bishopInt, paInt, pawnInt);
+            }
+            else if (paInt < biInt && pawnInt < bishopInt)
+            {
+                return DecreasingColumnDecreasingRow(biInt, bishopInt, paInt, pawnInt);
+            }
+            else if (paInt > biInt && pawnInt < bishopInt)
+            {
+                return IncreasingColumnDecreasingRow(biInt, bishopInt, paInt, pawnInt);
+            }
+            else if (paInt < biInt && pawnInt > bishopInt)
+            {
+                return DecreasingColumnIncreasingRow(biInt, bishopInt, paInt, pawnInt);
             }
 
-            while (loopVariable)
+            return false;
+        }
+
+        private static bool DecreasingColumnIncreasingRow(int biInt, int bishopInt, int paInt, int pawnInt)
+        {
+            while (biInt >=1  && bishopInt <= 8)
             {
-
+                biInt--;
+                bishopInt++;
+                if (biInt == paInt && bishopInt == pawnInt)
+                {
+                    return true;
+                }
             }
+            return false;
+        }
 
-            while (loopVariable)
+        private static bool IncreasingColumnDecreasingRow(int biInt, int bishopInt, int paInt, int pawnInt)
+        {
+            while (biInt <= 8 && bishopInt >= 1)
             {
-
+                biInt++;
+                bishopInt--;
+                if (biInt == paInt && bishopInt == pawnInt)
+                {
+                    return true;
+                }
             }
+            return false;
+        }
 
-            while (loopVariable)
+        private static bool DecreasingColumnDecreasingRow(int biInt, int bishopInt, int paInt, int pawnInt)
+        {
+            while (biInt >= 1 && bishopInt <= 8)
             {
-
+                biInt--;
+                bishopInt--;
+                if (biInt == paInt && bishopInt == pawnInt)
+                {
+                    return true;
+                }
             }
+            return false;
+        }
 
-
+        private static bool IncreasingColumnIncreasingRow(int biInt, int bishopInt, int paInt, int pawnInt)
+        {
+            while(biInt<=8 && bishopInt<=8)
+            {
+                biInt++;
+                bishopInt++;
+                if (biInt == paInt && bishopInt == pawnInt)
+                {
+                    return true;
+                }
+            }
             return false;
         }
     }

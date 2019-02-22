8902a5319a55f33e53b0bc8817132b792bec3055
Manoj Annam <annam.manoj1996@gmail.com>
2019-02-22 22:18:52 +0530

    Third one of the day

---
+++ ChessKnight/ChessKnightDirections/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessKnightDirections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cell = Console.ReadLine();
+            int res = chessKnight(cell);
+            Console.WriteLine();
+        }
+
+        private static int chessKnight(string cell)
+        {
+            int result = 0;
+            int column = cell[0];
+
+            int row = Convert.ToInt32(cell[1].ToString());
+            if(column-1 >=97)
+            {
+                if(row+2<=8)
+                {
+                    result++;
+                }
+                if (row - 2 >= 1)
+                {
+                    result++;
+                }
+            }
+            if (column - 2 >= 97)
+            {
+                if (row + 1 <= 8)
+                {
+                    result++;
+                }
+                if (row - 1 >= 1)
+                {
+                    result++;
+                }
+            }
+            if (column + 1 <= 104)
+            {
+                if (row + 2 <= 8)
+                {
+                    result++;
+                }
+                if (row - 2 >= 1)
+                {
+                    result++;
+                }
+            }
+            if (column + 2 <= 104)
+            {
+                if (row + 1 <= 8)
+                {
+                    result++;
+                }
+                if (row - 1 >= 1)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}

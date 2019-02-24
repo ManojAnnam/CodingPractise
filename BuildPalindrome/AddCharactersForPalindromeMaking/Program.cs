f8c2129b9488a580527966082012b20eec1ca334
Manoj Annam <annam.manoj1996@gmail.com>
2019-02-24 20:29:25 +0530

    Build_Palindrome

---
+++ BuildPalindrome/AddCharactersForPalindromeMaking/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddCharactersForPalindromeMaking
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string res = buildPalindrome(input);
+            Console.WriteLine();
+        }
+
+        private static string buildPalindrome(string input)
+        {
+            bool isPallindrome = true;
+            int i = 0;
+            string s = string.Empty;
+            while(isPallindrome)
+            {
+                s = input[i] + s;
+
+                isPallindrome = checkPallindrome(input+s);
+                if (isPallindrome)
+                {
+                    
+                    break;
+                }
+                i++;
+                isPallindrome = true;
+
+            }
+           
+            return input+s;
+        }
+
+        private static bool checkPallindrome(string input)
+        {
+            return input.SequenceEqual(input.Reverse());
+        }
+    }
+}

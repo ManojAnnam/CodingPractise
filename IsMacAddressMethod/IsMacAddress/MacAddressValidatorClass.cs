8120061589195e84c60c0cdeccf8bac8e2336096
Manoj Annam <annam.manoj1996@gmail.com>
2019-03-05 08:12:37 +0530

    MacAddressValidation

---
+++ IsMacAddressMethod/IsMacAddress/MacAddressValidatorClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IsMacAddress
+{
+    class MacAddressValidatorClass
+    {
+       
+        public bool IsMacAddress(string macAddressInput)
+        {
+            Regex onlyHexa = new Regex(@"^[0-9A-F]*$");
+            if (macAddressInput.Length==17)
+            {
+                string[] individualSubStrings = macAddressInput.Split('-');
+                if(!individualSubStrings.Any(c => c.Length!=2))
+                {
+                    if(individualSubStrings.All(d => onlyHexa.IsMatch(d)))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ IsMacAddressMethod/IsMacAddress/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsMacAddress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            MacAddressValidatorClass macAddressValidatorClass = new MacAddressValidatorClass();
+            bool isValid = macAddressValidatorClass.IsMacAddress(input);
+            Console.ReadLine();
+        }
+    }
+}
